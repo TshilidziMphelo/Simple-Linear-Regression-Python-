8e2cf3d843793939d719c73b9658a3c58619a980
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-04-11 20:05:42 +0200

    Splitting the dataframe into Training and Test data, then we create an instance of LinearRegressionModel and fit the training data to get a model. Then we use the predict function to make predictions off our model and we use the evaluate function to calculate the Mean Square Error to get the accuracy of the model

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,4 +1,6 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 class LinearRegressionModel:
@@ -29,3 +31,22 @@ X = df.iloc[:, :-1]
 
 # Select the target variable
 y = df.iloc[:, -1]
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Create an instance of the LinearRegressionModel class
+model = LinearRegressionModel()
+
+# Split the data into training and test sets
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = model.evaluate(y_test, y_pred)
+print(f'Mean Squared Error: {mse:.2f}')

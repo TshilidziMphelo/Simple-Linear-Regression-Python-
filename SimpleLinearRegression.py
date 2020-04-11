3f3a01226e76e76bb82666144d8d2bd174aba26f
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-04-11 14:55:19 +0200

    Cleaning and spliting the dataframe into X & Y axes

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -6,3 +6,12 @@ df = pd.read_csv('score.csv')
 # Explore the data
 print(df.head())
 print(df.describe())
+
+# Clean and prepare the data
+df = df.dropna()
+
+# Select the features
+X = df.iloc[:, :-1]
+
+# Select the target variable
+y = df.iloc[:, -1]

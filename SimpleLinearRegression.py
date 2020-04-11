de6bbf585da2db28e6f2609a3c31bc88ded7a71f
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-04-11 16:49:52 +0200

    Creating a LinearRegressionModel class with the necessary methods for our project

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,4 +1,18 @@
+from sklearn.linear_model import LinearRegression
 import pandas as pd
+
+class LinearRegressionModel:
+    def __init__(self):
+        self.model = LinearRegression()
+    
+    def fit(self, X, y):
+        self.model.fit(X, y)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    def evaluate(self, y_true, y_pred):
+        return mean_squared_error(y_true, y_pred)
 
 # Load the data into a pandas DataFrame
 df = pd.read_csv('score.csv')
@@ -7,7 +21,7 @@ df = pd.read_csv('score.csv')
 print(df.head())
 print(df.describe())
 
-# Clean and prepare the data
+# Cleaning the data
 df = df.dropna()
 
 # Select the features
